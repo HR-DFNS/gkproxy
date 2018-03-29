4083a17b2fd44b867d8c0e8873056b900470368d
girikalra <girikalra@gmail.com>
2018-03-29 16:39:25 -0700

    set up ssr with proxy

--- server.js
+++ server.js
@@ -1,30 +1,47 @@
 const express = require('express')
 const morgan = require('morgan');
 const path = require('path');
+const React = require('react');
+const ReactDom = require('react-dom/server');
 const app = express();
 const port = process.env.PORT || 3000;
 const request = require('request');
+const redis = require('redis');
+const Layout = require('./templates/layout');
+const App = require('./templates/app');
+const Scripts = require('./templates/scripts');
+//app.use(morgan('dev'));
 
-app.use(morgan('dev'));
-// app.use(express.static(path.join(__dirname, 'public')));
+const clientBundles = './public/services';
+const serverBundles = './templates/services';
+const serviceConfig = require('./service-config.json');
+const services = require('./loader.js')(clientBundles, serverBundles, serviceConfig);
+
+const renderComponents = (components, props = {}) => {
+  return Object.keys(components).map(item => {
+    let component = React.createElement(components[item]);
+    return ReactDom.renderToString(component);
+  });
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.redirect('/restaurants/ChIJUcXYWWGAhYARmjMY2bJAG2s/');
+  res.redirect('/restaurants/1/');
 });
 
-app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
+app.get('/restaurants/:id', function(req, res){
+  let components = renderComponents(services);
+  const rendered = Layout(
+    'WeGot',
+    App(...components),
+    Scripts(Object.keys(services))
+  );
+  res.end(rendered);
+});
 
-app.get('/api/restaurants/:id/gallery', (req, res) => {
-  res.redirect(`http://13.57.148.57/api/restaurants/${req.params.id}/gallery`)
-});
-app.get('/api/restaurants/:id/overview', (req, res) => {
-  res.redirect(`http://184.169.248.150/api/restaurants/${req.params.id}/overview`)
-});
 app.get('/api/restaurants/:id/sidebar', (req, res) => {
-  res.redirect(`http://54.177.233.239/api/restaurants/${req.params.id}/sidebar`)
-});
-app.get('/api/restaurants/:id/recommendations', (req, res) => {
-  res.redirect(`http://52.89.102.101/api/restaurants/${req.params.id}/recommendations`)
+  res.redirect(`http://localhost:3003/api/restaurants/${req.params.id}/sidebar`);
 });
 
 app.listen(port, () => {
---
+++ templates/app.js
@@ -0,0 +1,1 @@
+module.exports = (sidebar) => `<div id="Sidebar">${sidebar}</div>`;
